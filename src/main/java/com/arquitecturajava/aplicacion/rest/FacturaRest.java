85cd5676585ca248ff3f942be2fc997934b52a54
Nelson Trujillo <nelson@localhost.localdomain>
2020-04-01 21:17:03 -0500

    CECY:fix rest

--- src/main/java/com/arquitecturajava/aplicacion/rest/FacturaRest.java
+++ src/main/java/com/arquitecturajava/aplicacion/rest/FacturaRest.java
@@ -1,6 +1,17 @@
 package com.arquitecturajava.aplicacion.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Path("api")
 public class FacturaRest {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getTestService() {
+		return "Hello World! This is coming from webservice!!";
+	}
 
 }
